3984662de7734fdd68c688d5018a6d1e97f44185
Alice <alexandra.rudenko@jetbrains.com>
2019-03-03 18:36:49 +0300

    1

--- Socks/Sizes.cs
+++ Socks/Sizes.cs
@@ -8,7 +8,9 @@ namespace Socks
         public static List<Size> CreateSizes()
         {
             List<Size> sizes = new List<Size>();
-            
+
+            sizes.Add(new Size() {footLengthMin = 220, footLengthMax = 233.4, footLength = 226.5, start = 24, oneNeedle = 12, shoe1 = "35", shoe2 = "36"});            
+            sizes.Add(new Size() {footLengthMin = 233.5, footLengthMax = 246.9, footLength = 240, start = 24, oneNeedle = 13, shoe1 = "37", shoe2 = "38"});
             sizes.Add(new Size() {footLengthMin = 247, footLengthMax = 260, footLength = 253.5, start = 28, oneNeedle = 14, shoe1 = "39", shoe2 = "40"});
             sizes.Add(new Size() {footLengthMin = 260.1, footLengthMax = 273.5, footLength = 266.5, start = 28, oneNeedle = 14, shoe1 = "41", shoe2 = "42"});
             sizes.Add(new Size() {footLengthMin = 273.6, footLengthMax = 286.5, footLength = 280, start = 28, oneNeedle = 15, shoe1 = "43", shoe2 = "44"});
